2122b0f881df93c4f144b31d9353ed21034cd0ec
chschwer <45539899+chschwer@users.noreply.github.com>
2019-04-23 21:40:16 +0200

    Simple install script added

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Assumption: Called from within dotfiles-directory
+
+dotdir=$(pwd)
+
+ln -sf $dotdir/gitconfig ~/.gitconfig
+ln -sf $dotdir/vimrc ~/.vimrc
+ln -sf $dotdir/editorconfig ~/.editorconfig
+ln -sf $dotdir/eslintrc ~/.eslintrc
+
